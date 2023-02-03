a8ef686675cfa1be18256106e9a27c2b344a3f91
Javier Vicedo <101125117+Viseiras@users.noreply.github.com>
2023-02-03 12:30:09 +0100

    Creación de la forma Añadir

---
+++ GameCore/FormAñadir.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameCore
+{
+    public partial class FormAñadir : Form
+    {
+        public FormAñadir()
+        {
+            InitializeComponent();
+        }
+    }
+}
